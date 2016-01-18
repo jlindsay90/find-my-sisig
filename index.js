e52c3d99f46a4d7b3bfdc91db1e4f160842d57c6
Josh Lindsay <jlindsay90@gmail.com>
2016-01-18 04:04:46 -0800

    fixed scraping - now nodejs issues

--- index.js
+++ index.js
@@ -13,30 +13,65 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 var logger = function(req, res, next) {
-  console.log('VISITED');
   console.log(new Date().toISOString() + " " + req.connection.remoteAddress);
   next();
 }
 app.use(logger);
 
-function getSisigDates() {
-  dates = {}
-  request('http://senorsisig.com/', function(err, res, body) {
-    if (!err && res.statusCode == 200) {
-      var $ = cheerio.load(body);
-      $('section').each(function(i, elem) {
-          var date = elem.data('wcal-date');
-          if (date && date != 'error')
-              dates[date] = 'HELLO';
-      });
-    }
-  });
-  return dates;
+function getDates(response) {
+  return function(e, xhr, body) { getSisigDates(response, e, xhr, body); }
 }
 
-app.get('/', function(request, response) {
-  console.log(request.remote)
-  response.send(getSisigDates());
+/*
+function add(dict, key, value) {
+    if (!dict[key])
+        dict[key] = [];
+    dict[key].push(value);
+}
+*/
+
+function getSisigDates(response, err, res, body) {
+  var table = document.createElement('table');
+
+  //var dates = {};
+  if (!err && res.statusCode == 200) {
+    var $ = cheerio.load(body);
+    var sections = $('section');
+    sections.each(function(i, elem) {
+      var date = $(this).data('wcal-date');
+      if (date && date != 'error') {
+        var row = document.createElement('tr');
+        var dayCell = document.createElement('td');
+        var timeCell = document.createElement('td');
+        var locCell = document.createElement('td');
+
+        var loc = $(this).find('a.map-trigger').text().trim();
+        var day = $(this).find('div.date').text().replace(/\b(\w{3})\b/, "$1");
+        var time = $(this).find('div.time').text().trim().replace(/(\w+)[\s\n]*to[\n\s]*(\w+)/, "$1 to $2");
+        //add(dates, date, [loc, time]);
+
+        dayCell.appendChild(document.createTextNode(day + " " + date));
+        timeCell.appendChild(document.createTextNode(time));
+        locCell.appendChild(document.createTextNode(loc));
+
+        row.appendChild(dayCell);
+        row.appendChild(timeCell);
+        row.appendChild(locCell);
+      }
+    });
+  }
+  response.send(table);
+}
+
+app.get('/', function(req, res) {
+  var options = {
+    url:"http://senorsisig.com/",
+    headers:{
+      'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0 Iceweasel/45.0a2',
+      'Accept':'text/html;q=0.9,*;q=0.8'
+    }
+  };
+  request(options, getDates(res));
 });
 
 app.listen(app.get('port'), function() {
