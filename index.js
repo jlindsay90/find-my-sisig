c916165c56f3dfb6aba324b17e02c818eda0039d
Josh Lindsay <jlindsay90@gmail.com>
2016-01-18 01:16:39 -0800

    added basic request - not working

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var request = require('request');
+var cheerio = require('cheerio');
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -9,8 +12,31 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+var logger = function(req, res, next) {
+  console.log('VISITED');
+  console.log(new Date().toISOString() + " " + req.connection.remoteAddress);
+  next();
+}
+app.use(logger);
+
+function getSisigDates() {
+  dates = {}
+  request('http://senorsisig.com/', function(err, res, body) {
+    if (!err && res.statusCode == 200) {
+      var $ = cheerio.load(body);
+      $('section').each(function(i, elem) {
+          var date = elem.data('wcal-date');
+          if (date && date != 'error')
+              dates[date] = 'HELLO';
+      });
+    }
+  });
+  return dates;
+}
+
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  console.log(request.remote)
+  response.send(getSisigDates());
 });
 
 app.listen(app.get('port'), function() {
